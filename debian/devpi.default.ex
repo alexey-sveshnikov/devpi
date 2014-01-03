12733a85d44b57c96ec1511fc1d6cd375557044b
Alexey Sveshnikov <alexey.sveshnikov@gmail.com>
2014-01-04 01:52:27 +0400

    Initial version of devpi package

---
+++ debian/devpi.default.ex
@@ -0,0 +1,10 @@
+# Defaults for devpi initscript
+# sourced by /etc/init.d/devpi
+# installed at /etc/default/devpi by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
---
+++ debian/menu.ex
@@ -0,0 +1,2 @@
+?package(devpi):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="devpi" command="/usr/bin/devpi"
